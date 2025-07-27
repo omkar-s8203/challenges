dd4af882489c354af5bb87cb6de32ddf4b992638
omkar-s8203 <omkar876742@gmail.com>
2025-07-27 12:52:59 +0530

    Add implementations for Butterfly Pattern and Solid Rhombus

--- src/Triangle01Pattern.java
+++ src/Triangle01Pattern.java
@@ -1,2 +1,21 @@
 public class Triangle01Pattern {
+    public static void main(String[] args) {
+triangle01(5);
+    }
+
+    public static void triangle01(int n){
+    for (int i = 1; i <= n; i++){
+
+        //inner loop
+        for (int j = 1; j <= i; j++){
+            if((i+j)%2==0){
+                System.out.print("1");
+            }else {
+                System.out.print("0");
+            }
+        }
+
+        System.out.println();
+    }
+    }
 }
